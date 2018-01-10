359136a1c11e0fd3740cddfc74bdb3378f6d2697
JamesM <Jayt.v@hotmail.com>
2018-01-10 11:52:35 +0000

    upload

---
+++ CrossPlatformApp/CrossPlatformApp/SimpleContentPage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace CrossPlatformApp
+{
+	public class SimpleContentPage : ContentPage
+	{
+		public SimpleContentPage ()
+		{
+            var lable1 = new Label
+            {
+                Text = "Test Label",
+                Font = Font.BoldSystemFontOfSize(NamedSize.Large)
+
+            };
+
+            var entry1 = new Entry
+            {
+                Placeholder = "Type Here"
+            };
+
+            var button1 = new Button
+            {
+                Text = "test button"
+                                
+            };
+
+            button1.Clicked += (sender, e) =>
+            {
+                lable1.Text = "User types " + entry1.Text;
+            };
+
+            Content = new StackLayout
+            {
+                Padding = 30,
+
+                Spacing = 10,
+                Children = { lable1, entry1, button1 }
+            };
+		}
+
+        
+    }
+}
